5acc5756a29b544a831836241e66e72f8a97944f
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-10 16:26:47 +0530

    Update emp_sal.py

--- emp_sal.py
+++ emp_sal.py
@@ -1,5 +1,14 @@
-salary = float(sys.argv[1])
-bonus = 0.10 * salary
-total_sal = salary + bonus
+import sys
+
+if len(sys.argv) == 2:
+  script_salary = sys.argv[0]
+  salary = sys.argv[1]
+
+else:
+  script_salary.argv[0]
+  salary = float(sys.argv[1])
+  bonus = 0.10*salary
+  total_salary = salary*bonus
+
 print("Bonus Amount : Rs.",bonus)
 print("Total Salary : Rs.",total_sal)
