4d3f8afdc09c11c31d6057e19699e34c2cc7a89b
Gaurav Kattimani <gaurav.kat024@gmail.com>
2025-11-10 16:13:52 +0530

    Update emp_sal.py

--- emp_sal.py
+++ emp_sal.py
@@ -1,4 +1,4 @@
-salary=float(input("Enter the Employee's salary : "))
+salary = float(input("Enter the Employee's salary : "))
 bonus = 0.10 * salary
 total_sal = salary + bonus
 print("Bonus Amount : Rs.",bonus)
